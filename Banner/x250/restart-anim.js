245075b5457eb0bab3963daa4c85ae56b09b9ece
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2024-04-26 13:08:17 +0700

    banner

--- Banner/x250/restart-anim.js
+++ Banner/x250/restart-anim.js
@@ -1,40 +1,27 @@
 function restartAnimations() {
   setTimeout(function () {
-    document.getElementById("question").style.animation = "none";
-    document.getElementById("answer").style.animation = "none";
-    document.getElementById("phone-content").style.animation = "none";
-    document.getElementById("group").style.animation = "none";
-    document.getElementById("pin8").style.animation = "none";
+    document.getElementById("content-text").style.animation = "none";
+    document.getElementById("phone-container").style.animation = "none";
+    document.getElementById("rating-box").style.animation = "none";
     document.getElementById("screen").style.animation = "none";
     document.getElementById("logo").style.animation = "none";
     document.getElementById("dummy").style.animation = "none";
 
     void document.body.offsetWidth;
 
-    document.getElementById("question").style.animation =
-      "0.7s showBox 0.5s ease forwards, 0.7s showBox 7s ease forwards reverse";
-
-    document.getElementById("answer").style.animation =
-      "0.7s showBox 0.5s ease forwards, 0.7s showBox 7s ease forwards reverse";
-
-    document.getElementById("phone-content").style.animation =
-      "0.7s showPhone 1s ease forwards, 0.7s showPhone 7s ease forwards reverse";
-
-    document.getElementById("group").style.animation =
-      "1s opacity 1.5s ease forwards";
-
-    document.getElementById("pin8").style.animation =
-      "0.7s scale 2.5s ease forwards";
-
+    document.getElementById("content-text").style.animation =
+      "0.7s showBox 2s ease forwards";
+    document.getElementById("phone-container").style.animation =
+      "0.7s showPhone 3s ease forwards";
+    document.getElementById("rating-box").style.animation =
+      "1s rating-box 4s ease forwards";
     document.getElementById("screen").style.animation =
-      "1s showScreen 5s ease forwards, 0.7s showScreen 7.5s ease forwards reverse";
-
+      "0.7s showScreen 1s ease forwards reverse, 1s showScreen 15s ease forwards";
     document.getElementById("logo").style.animation =
-      "0.7s logoScale 5.1s ease forwards, 0.7s logoScale 7.5s ease forwards reverse";
-
+      "0.7s logoScale 1s ease forwards reverse, 0.7s logoScale 15s ease forwards";
     document.getElementById("dummy").style.animation =
-      "dummyAnimation 7.5s ease forwards";
-  }, 700);
+      "dummyAnimation 15s ease forwards";
+  }, 1000);
 }
 
 document
