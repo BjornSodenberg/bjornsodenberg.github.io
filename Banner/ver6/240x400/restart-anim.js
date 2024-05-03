5c97ce6c401d329b65a1fcc12875f2b0f0aa70b2
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2024-05-03 10:49:09 +0700

    banner

--- Banner/ver6/240x400/restart-anim.js
+++ Banner/ver6/240x400/restart-anim.js
@@ -3,7 +3,7 @@ function restartAnimations() {
     document.getElementById("logo").style.animation = "none";
     document.getElementById("first-title").style.animation = "none";
     document.getElementById("second-title").style.animation = "none";
-    document.getElementById("third-title").style.animation = "none";
+    // document.getElementById("third-title").style.animation = "none";
     document.getElementById("first-text").style.animation = "none";
     document.getElementById("second-text").style.animation = "none";
     document.getElementById("disclaimer").style.animation = "none";
@@ -15,7 +15,7 @@ function restartAnimations() {
     document.getElementById("logo").style.animation = "0.4s rightLeft 0.4s ease forwards, 0.4s rightLeft 7s ease forwards reverse";
     document.getElementById("first-title").style.animation = "0.4s rightLeft 1.4s ease forwards, 0.4s rightLeft 7s ease forwards reverse";
     document.getElementById("second-title").style.animation = "0.4s rightLeft 1.6s ease forwards, 0.4s rightLeft 7s ease forwards reverse";
-    document.getElementById("third-title").style.animation = "0.4s rightLeft 1.8s ease forwards,0.4s rightLeft 7s ease forwards reverse";
+    // document.getElementById("third-title").style.animation = "0.4s rightLeft 1.8s ease forwards,0.4s rightLeft 7s ease forwards reverse";
     document.getElementById("first-text").style.animation = "0.4s bottomTop 2.8s ease forwards, 0.4s bottomTop 7s ease forwards reverse";
     document.getElementById("second-text").style.animation = "0.4s bottomTop 3s ease forwards, 0.4s bottomTop 7s ease forwards reverse";
     document.getElementById("disclaimer").style.animation = "0.4s bottomTop 4s ease forwards, 0.4s bottomTop 7s ease forwards reverse";
